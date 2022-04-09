eda68888be8677dbed4ffafeceb54420a4861a4c
Flavio Benedini <flavio.benedini@me.com>
2022-04-09 11:53:36 -0300

    feat: Adicionando o listen do servidor na porta 3000

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ const port = 3000
 
 app.get('/', (req, res) => res.sendFile(path.join(___dirname, 'views/index.html')))
 app.get('/sobre', (req, res) => res.sendFile(path.join(___dirname, 'views/sobre.html')))
+
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
